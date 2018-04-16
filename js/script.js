0f14dd5e699d858a2ab2398873f38f567922b1a4
salvatorescarfone <34183712+salvatorescarfone@users.noreply.github.com>
2018-04-16 21:53:03 +0200

    JSON aggiunto

--- js/script.js
+++ js/script.js
@@ -1,144 +1,88 @@
 var moveX=0, moveY=0;
-function drawPacman(){
-  $("canvas").drawImage({
-    source:"img/pacman.png",
-    x:250,y:280,
-    sx:0,sy:0,
-    sWidth:15,sHeight:17,
-    width:25,height:25
-  });
-}
-
-function drawMaze(){
-  $("canvas").drawImage({
-    source:"img/maze.png",
-    x:250,y:250,
-    width:400,height:450
-  });
-}
-
-function create2DArray(rows) {
-  var matrix = [];
-  for(var i=0; i<rows; i++) {
-      matrix[i] = [];
-      for(var j=0; j<rows; j++) {
-          matrix[i][j] = 0;
-      }
-  }
-  return matrix;
-}
 
 $(document).ready(function() {
 
+  //id = setInterval(update,10);
 
-  id = setInterval(update,10);
+  var x = new Pacman(0,0,25,25);
 
-  var dx=2,dy=2;
+  //x.move();
 
+  var dx=0.8,dy=0.8;
 
-    /*
-    37 sx
-    38 up
-    39 dx
-    40 down
-    */
+/*
+
     $(document).keydown(function(event) {
       console.log(event.which);
       switch (event.which) {
-        case 37:
+        case 37://sx
           moveX=-dx;
           moveY= 0;
-          pacman.tx = 3;
-          pacman.ty = 2;
+          pacman.tx = pacman[3].tx;
+          pacman.ty = pacman[3].ty;
           break;
-        case 38:
+        case 38://su
           moveY=-dy;
           moveX= 0;
-          pacman.tx = 2;
-          pacman.ty = 43;
+          pacman.tx = pacman[1].tx;
+          pacman.ty = pacman[1].ty;
           break;
-        case 39:
+        case 39://dx
           moveX= dx;
           moveY= 0;
-          pacman.tx = 2;
-          pacman.ty = 22;
+          pacman.tx = pacman[2].tx;
+          pacman.ty = pacman[2].ty;
           break;
-        case 40:
+        case 40://giu
           moveY= dy;
           moveX= 0;
-          pacman.tx = 3;
-          pacman.ty = 62;
+          pacman.tx = pacman[4].tx;
+          pacman.ty = pacman[4].ty;
           break;
         case 27:
           clearInterval(id);
       }
 
     });
-
+*/
 });
 
-var pacman = {
-  "type":"pacman",
-  "x":25,"y":25,
-  "tx":2,"ty":22,
-  "tw":14,"th":15,
-  "w":20,"h":20
-}
-
-var pacmans = {
-  "type":"pacmandw",
-  "tx":3,
-  "ty":62,
-  "tw":14,
-  "th":14
-}
-
-var line = {
-  "type":"line",
-  "x":200,"y":25,
-  "tx":119,"ty":384,
-  "tw":10,"th":49,
-  "w":9,"h":48
-}
-
+/*
 function collision(){
 
-
   var f = false;
-  if(pacman.x+pacman.w/2>line.x-line.w/2)//solo controllo per la x
+  if((pacman.x+pacman.w/2 > Math.abs(line.x-line.w/2))&&(true))//solo controllo per la x
     f = true;
 
     console.log(line.y - line.h/2);
 
   return f;
-}
+}*/
 
 function update(){
 
+
   $("canvas").css('background-color', 'black');
-  $('canvas').clearCanvas();
+  $("canvas").removeLayers();
+
+  //pacman.x += moveX;
+  //pacman.y += moveY;
 
   //console.log("x: "+pacman.x+"     y: "+pacman.y);
 
-  if(!collision()){
-    pacman.x += moveX;
-    pacman.y += moveY;
-  }
-  $("canvas").drawImage({
-    source:"img/spritesheet.png",
-    x:pacman.x,y:pacman.y,
-    sx:pacman.tx,sy:pacman.ty,
-    sWidth:pacman.tw,sHeight:pacman.th,
-    width:pacman.w,height:pacman.h
-  });
+  //if(!collision()){
 
-  $("canvas").drawImage({
-    source:"img/pacmanGridlines.png",
-    x:line.x,y:line.y,
-    sx:line.tx+2,sy:line.ty+2,
-    sWidth:line.tw-3,sHeight:line.th-3,
-    width:line.w,height:line.h
-  });
+  //}
+
+  $('canvas').addLayer({
+    type: 'image',
+    source: 'SheetV1.png',
+    x:25,y:25
+});
+
+
+  $('canvas').drawLayers();
+
 
 
 

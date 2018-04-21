f121e3a7d96d9734b5e52693ef17de0aca220118
salvatorescarfone <34183712+salvatorescarfone@users.noreply.github.com>
2018-04-21 10:37:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,50 +2,21 @@ var moveX=0, moveY=0;
 
 $(document).ready(function() {
 
-  //id = setInterval(update,10);
-
-  var x = new Pacman(0,0,25,25);
+  id = setInterval(update,10);
 
   //x.move();
 
   var dx=0.8,dy=0.8;
 
-/*
 
-    $(document).keydown(function(event) {
+
+  $(document).keydown(function(event) {
       console.log(event.which);
-      switch (event.which) {
-        case 37://sx
-          moveX=-dx;
-          moveY= 0;
-          pacman.tx = pacman[3].tx;
-          pacman.ty = pacman[3].ty;
-          break;
-        case 38://su
-          moveY=-dy;
-          moveX= 0;
-          pacman.tx = pacman[1].tx;
-          pacman.ty = pacman[1].ty;
-          break;
-        case 39://dx
-          moveX= dx;
-          moveY= 0;
-          pacman.tx = pacman[2].tx;
-          pacman.ty = pacman[2].ty;
-          break;
-        case 40://giu
-          moveY= dy;
-          moveX= 0;
-          pacman.tx = pacman[4].tx;
-          pacman.ty = pacman[4].ty;
-          break;
-        case 27:
-          clearInterval(id);
-      }
 
-    });
-*/
-});
+      //if(event.which == 27) Arena.pause();
+      pacman.setDir(event.which);
+
+  });
 
 /*
 function collision(){
@@ -58,6 +29,8 @@ function collision(){
 
   return f;
 }*/
+
+
 
 function update(){
 
@@ -76,7 +49,7 @@ function update(){
 
   $('canvas').addLayer({
     type: 'image',
-    source: 'SheetV1.png',
+    source: 'img/SheetV1.png',
     x:25,y:25
 });
 
